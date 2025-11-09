5d45a982e61a344a0df3824d75a7580fc7118a61
BXAMRA <jassbhamra@outlook.com>
2025-11-09 09:05:02 +0530

    v1.0.0

---
+++ bin/flutter_post_build.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_post_build/flutter_post_build.dart' as flutter_post_build;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_post_build.calculate()}!');
+}
---
+++ test/flutter_post_build_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_post_build/flutter_post_build.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
